f0f523637ea53d69132d1856904f8ffd6ef89eb4
froeh010 <froeh010@uni-hildesheim.de>
2020-09-18 13:47:43 +0200

    Initial commit after moving from simplic-service

---
+++ src/Simplic.Authentication/Exception/LoginFailedException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Simplic.Authentication
+{
+    /// <summary>
+    /// Exception that will be thrown, if the login process fails
+    /// </summary>
+    public class LoginFailedException : Exception
+    {
+        /// <summary>
+        /// Create login failed exception
+        /// </summary>
+        /// <param name="type">Login failed type</param>
+        public LoginFailedException(LoginFailedType type) : base("Login failed")
+        {
+            Type = type;
+        }
+
+        /// <summary>
+        /// Gets or sets the login failed type
+        /// </summary>
+        public LoginFailedType Type
+        {
+            get;
+            set;
+        }
+    }
+}
